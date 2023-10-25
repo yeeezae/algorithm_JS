e73c04d119fd7bda91d978104b7ad5e32d7a59c1
kjjlss2 <jgim0417@gmail.com>
2023-10-25 23:07:16 +0900

    rename: Programmes Lv0 1-2일차 폴더 이동

---
+++ Programmers/day_01.js
@@ -0,0 +1,26 @@
+//변수의 이해
+//Solution 함수 이해
+
+//01. 두수의 합
+function solution(num1,num2) {
+  return num1 + num2
+}
+
+//02. 두수의 차
+function solution(num1,num2) {
+  var answer = num1 - num2
+  return answer
+}
+
+//03. 두수의 곱
+function solution(num1,num2) {
+  var answer = num1 * num2
+  return answer
+}
+
+//04. 두수의 몫
+//Math.floor를 이용해 버림을 사용할 수 있다.
+function solution(num1,num2) {
+  var answer = Math.floor(num1 / num2)
+  return answer
+}
